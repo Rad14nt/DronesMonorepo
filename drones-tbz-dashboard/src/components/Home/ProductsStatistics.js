d42a74e4612d57ec7a9f1ce298a87a5d97b963d5
Andrei Mititelu <andrei@gimli.ro>
2024-11-14 16:57:44 +0100

    Remove submodule references and add project files as regular directories

---
+++ drones-tbz-dashboard/src/components/Home/ProductsStatistics.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const ProductsStatistics = () => {
+  return (
+    <div className="col-xl-6 col-lg-12">
+      <div className="card mb-4 shadow-sm">
+        <article className="card-body">
+          <h5 className="card-title">Products statistics</h5>
+          <iframe title="Frame"
+            style={{
+              background: "#FFFFFF",
+              border: "none",
+              borderRadius: "2px",
+              boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2);",
+              width: "100%",
+              height: "350px",
+            }}
+            src="https://charts.mongodb.com/charts-drones-lcymw/embed/charts?id=63d8486e-9323-49ad-86a5-1f2c52a80e5d&maxDataAge=3600&theme=light&autoRefresh=true"
+          ></iframe>
+        </article>
+      </div>
+    </div>
+  );
+};
+
+export default ProductsStatistics;
